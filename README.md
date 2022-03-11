c9d7f45baa24b9e8d586e86941450d99ca7d4576
Linimik <jameskimimk@gmail.com>
2022-03-10 20:24:37 -0500

    Added typescript and nodemon as package depencieices

--- README.md
+++ README.md
@@ -3,13 +3,12 @@
 
 #### To run:
 1. Make sure that the latest [Node](https://nodejs.org/en/download/) and npm versions are installed
-2. Make sure `typescript@next` is installed globally (Install using `npm i typescript@next -g`)'
-3. Clone and open the git repo
-4. Run `npm i` to install the required dependencies
+2. Clone and open the git repo
+3. Run `npm i` to install the required dependencies
     > (if you get an error while installing `quick.db`, open a terminal with admin permissions and try again)
-5. Follow the instructions [here](config.md)
-6. Run `tsc` to convert all `.ts` files to `.js` ones
-7. Use `node .` to start the application up!
+4. Follow the instructions [here](config.md)
+5. Run `npm run tsc` to convert all `.ts` files to `.js` ones
+6. Use `node .` to start the application up!, or use `npm run dev` to run a auto-reloading application.
 
 ### Some stuff:
  - All js files within the [commands folder](bot/commands) with a default export are seen as a command. The skeleton to a command can be seen [here](bot/commands/command.ts)
--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-import { Client, Intents } from "discord.js"
 import { REST } from "@discordjs/rest"
+import chalk from "chalk"
 import { Routes } from "discord-api-types/v9"
+import type { Queue } from "discord-player"
+import { Player } from "discord-player"
+import { Client, CommandInteraction, Intents } from "discord.js"
 import type { Command } from "./bot/commands/command.js"
 import { walkCommands, walkEvents } from "./bot/walk.js"
 import config from "./config.js"
-import chalk from "chalk"
 
 /* ------------------------------ Client setup ------------------------------ */
 interface NewClient extends Client<boolean> { commands: Map<string, Command> }
@@ -18,6 +20,16 @@ const client = <NewClient>new Client({
 client.commands = new Map()
 await walkCommands(client)
 await walkEvents(client)
+
+/* -------------------------------- For music ------------------------------- */
+// Create a new Player (you don't need any API Key)
+const player = new Player(client)
+
+// add the trackStart event so when a song will be played this message will be sent
+player.on("trackStart", (queue: Queue<CommandInteraction<CacheType>>, track) => {
+    queue.metadata
+    queue.metadata.channel.send(`🎶 | Now playing **${track.title}**!`)
+})
 
 /* ------------------------------- For testing ------------------------------ */
 const rest = new REST({ version: "9" }).setToken(config.token)
