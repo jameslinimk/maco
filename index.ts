f60bbd431b6bb46c909a0a9cead2c00d0de1c055
Linimik <jameskimimk@gmail.com>
2022-03-15 17:32:57 -0400

    Some perfomance measuring and spread syntax usage

--- index.ts
+++ index.ts
@@ -19,20 +19,28 @@ const client = <NewClient>new Client({
     ]
 })
 client.commands = new Map()
+const musicStart = performance.now()
+console.log(chalk.red("Started music setup"))
 musicSetup(client)
+console.log(`${chalk.red("Successfully set up music")} (${performance.now() - musicStart}ms)\n`)
+
+const walkStart = performance.now()
+console.log(chalk.red("Started walking through commands and events"))
 await Promise.all([walkCommands(client, "./bot/commands"), walkEvents(client, "./bot/events")])
+console.log(`${chalk.red("Successfully walked through commands and events")} (${performance.now() - walkStart}ms)\n`)
 
 /* ------------------------------- For testing ------------------------------ */
 const rest = new REST({ version: "9" }).setToken(config.token)
 try {
-    console.log(chalk.red("Started refreshing application (/) commands."))
+    const refreshStart = performance.now()
+    console.log(chalk.red("Started refreshing application (/) commands"))
 
     await rest.put(
         Routes.applicationGuildCommands(config.clientId, config.testGuildId),
-        { body: Array.from(client.commands.values()).map(command => command.dataBuilder.toJSON()) },
+        { body: [...client.commands.values()].map(command => command.dataBuilder.toJSON()) },
     )
 
-    console.log(chalk.red("Successfully reloaded application (/) commands.\n"))
+    console.log(`${chalk.red("Successfully reloaded application (/) commands")} (${performance.now() - refreshStart}ms)\n`)
 } catch (error) {
     console.error(error)
 }
